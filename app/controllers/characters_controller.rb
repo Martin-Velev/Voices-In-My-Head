31b23904f781db4d0ace7420bacd248bd6c807c5
Martin-Velev <jojo.nedich@gmail.com>
2017-01-07 21:27:23 +0200

    made form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,23 @@
 class CharactersController < ApplicationController
   def index
   end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to root_path
+    end
+
+  end
+
+
+  private
+    def character_params
+      params.require(:character).permit(:name, :description)
+    end
+
 end

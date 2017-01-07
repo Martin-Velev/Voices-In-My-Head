054ac37f3d094addd6b3facf689f5b05fe84d0ff
Martin-Velev <jojo.nedich@gmail.com>
2017-01-07 23:09:00 +0200

    Works!

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,23 +1,53 @@
 class CharactersController < ApplicationController
+
+  before_action :find_character, only: [:show, :edit, :update, :destroy]
   def index
+    @characters = Character.all.order("created_at DESC")
   end
 
   def new
     @character = Character.new
   end
 
+  def show
+  end
+
   def create
     @character = Character.new(character_params)
     if @character.save
       redirect_to root_path
+    else
+      render 'new'
     end
 
   end
+
+  def edit
+  end
+
+  def update
+    if @character.update(character_params)
+      redirect_to character_path(@character)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @character.destroy
+    redirect_to root_path
+  end
+
 
 
   private
     def character_params
       params.require(:character).permit(:name, :description)
     end
+
+    def find_character
+      @character = Character.find(params[:id])
+    end
+
 
 end
